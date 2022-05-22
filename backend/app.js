c235d08769ec4405047d43394e0855f2943c43bc
Julien <jujufrancois1999@gmail.com>
2022-05-22 11:58:04 +0200

    Connect to MongoDB

--- backend/app.js
+++ backend/app.js
@@ -1,58 +1,75 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const path = require('path');
 
 const Thing = require('./models/thing');
 const thing = require('./models/thing');
 
-mongoose.connect('mongodb+srv://julien:hEosHViy4T3WpxJx@nodeapp.n6tjj.mongodb.net/test?retryWrites=true&w=majority', {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-    })
-    .then(() => console.log('Connexion à MongoDB réussie !'))
-    .catch(() => console.log('Connexion à MongoDB échouée !'));
+mongoose
+	.connect(
+		'mongodb+srv://julien:wow1234@cluster0.fuoo0.mongodb.net/?retryWrites=true&w=majority', //Add your connection string from MongoDB
+		{ useNewUrlParser: true, useUnifiedTopology: true }
+	)
+	.then(() => console.log('Connection à MongoDB réussie'))
+	.catch((err) => console.log('Connection à MongoDB échouée', err));
 
 const app = express();
 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-    next();
+	res.setHeader('Access-Control-Allow-Origin', '*');
+	res.setHeader(
+		'Access-Control-Allow-Headers',
+		'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization'
+	);
+	res.setHeader(
+		'Access-Control-Allow-Methods',
+		'GET, POST, PUT, DELETE, PATCH, OPTIONS'
+	);
+	next();
 });
 
 app.use(bodyParser.json());
 
 app.post('/api/stuff', (req, res, next) => {
-    delete req.body._id;
-    const thing = new Thing({
-        ...req.body
-    });
-    thing.save()
-        .then(() => res.status(201).json({
-            message: 'Objet enregistré !'
-        }))
-        .catch(error => res.status(400).json({
-            error
-        }));
+	delete req.body._id;
+	const thing = new Thing({
+		...req.body,
+	});
+	thing
+		.save()
+		.then(() =>
+			res.status(201).json({
+				message: 'Objet enregistré !',
+			})
+		)
+		.catch((error) =>
+			res.status(400).json({
+				error,
+			})
+		);
 });
 
 app.get('/api/stuff/:id', (req, res, next) => {
-    Thing.findOne({
-            _id: req.params.id
-        })
-        .then(thing => res.status(200).json(thing))
-        .catch(error => res.status(404).json({
-            error
-        }));
+	Thing.findOne({
+		_id: req.params.id,
+	})
+		.then((thing) => res.status(200).json(thing))
+		.catch((error) =>
+			res.status(404).json({
+				error,
+			})
+		);
 });
 
 app.get('/api/stuff', (req, res, next) => {
-    Thing.find()
-        .then(things => res.status(200).json(things))
-        .catch(error => res.status(400).json({
-            error
-        }));
+	Thing.find()
+		.then((things) => res.status(200).json(things))
+		.catch((error) =>
+			res.status(400).json({
+				error,
+			})
+		);
 });
 
 module.exports = app;
--- backend/models/thing.js
+++ backend/models/thing.js
@@ -1,4 +1,4 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const thingSchema = mongoose.Schema({
   title: { type: String, required: true },
@@ -8,4 +8,4 @@ const thingSchema = mongoose.Schema({
   price: { type: Number, required: true },
 });
 
-module.exports = mongoose.model('Thing', thingSchema);
+module.exports = mongoose.model("Thing", thingSchema);
